75670eb7aa92b263c4b90a926b4cc5595f6d644f
Natalya Podorozhnaya <dragonam1986@gmail.com>
2021-08-03 17:13:22 +0300

    Added Post and WallService now

---
+++ src/main/kotlin/WallService.kt
@@ -0,0 +1,10 @@
+package ru.netology
+
+object WallService {
+    private var posts = emptyArray<Post>()
+
+    fun add(post: Post): Post {
+        posts += post
+        return posts.last()
+    }
+}
